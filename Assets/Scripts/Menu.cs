6208fee1c2939b1715486036133bf1e80d11f8b0
Jayderade <jcastleden@gmail.com>
2017-09-01 13:56:58 +1000

    Hardcode

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -8,6 +8,7 @@ public class Menu : MonoBehaviour
     [Header("Screen Elements")]
     public bool showOptions;
     public int scrW, scrH;
+    public bool fullScreenToggle;
     [Header("Keys")]
     public KeyCode forward;
     public KeyCode backward;
@@ -22,12 +23,16 @@ public class Menu : MonoBehaviour
     public KeyCode holdingKey;
     [Header("Resolutions")]
     public int index;
+    public bool showRes;
     public int[] resX, resY;
+    private Vector2 scrollPosRes;
     [Header("References")]
     public AudioSource mainMusic;
     public float volumeSlider;
     public Light brightness;
     public float brightnessSlider;
+    public float holdingVolume;
+    public bool muteToggle;
 
     void OnGUI()
     {
@@ -60,26 +65,124 @@ public class Menu : MonoBehaviour
             int i = 0;
             GUI.Box(new Rect(4 * scrW, 0.25f * scrH, 8 * scrW, 2 * scrH), "Options");//Title
 
-            GUI.Box(new Rect(0.5f * scrW, 3 * scrH + (i * 0.75f* scrH), scrW, 0.75f* scrH), "Volume");
-            volumeSlider = GUI.HorizontalSlider(new Rect(2 * scrW, 3.25f* scrH + (i * 1f * scrH), 2f * scrW, 0.25f * scrH), volumeSlider, 0, 1);
+            GUI.Box(new Rect(0.5f * scrW, 3 * scrH + (i * 0.75f* scrH),2 * scrW, 0.75f* scrH), "Volume");
+            volumeSlider = GUI.HorizontalSlider(new Rect(3 * scrW, 3.25f* scrH + (i * 1f * scrH), 2f * scrW, 0.25f * scrH), volumeSlider, 0, 1);
+
+            if(GUI.Button(new Rect(5.75f * scrW, 3 * scrH + (i * (scrH * 0.5f)),1.5f * scrW, 0.5f * scrH), "Mute")) // Label
+            {
+                ToggleVolume();
+            }
+
             i++;
-            GUI.Box(new Rect(0.5f * scrW, 3 * scrH + (i * 0.75f* scrH), scrW, 0.75f* scrH), "Brightness");
-           brightnessSlider = GUI.HorizontalSlider(new Rect(2 * scrW, 3.25f * scrH + (i * .75f* scrH), 2f * scrW, 0.25f * scrH),brightnessSlider, 0, 1);
+            GUI.Box(new Rect(0.5f * scrW, 3 * scrH + (i * 0.75f* scrH),2 * scrW, 0.75f* scrH), "Brightness");
+           brightnessSlider = GUI.HorizontalSlider(new Rect(3 * scrW, 3.25f * scrH + (i * .75f* scrH), 2f * scrW, 0.25f * scrH),brightnessSlider, 0, 1);
 
+            #region Resoulution and Screen
+            i++;
+            if(GUI.Button(new Rect(0.5f * scrW, 3 * scrH + (i * 0.75f * scrH), 2 * scrW, 0.5f * scrH), "Resolutions"))
+            {
+                showRes = !showRes;
+            }
 
+            if (GUI.Button(new Rect(2f * scrW, 3 * scrH + (i * 0.75f * scrH), 1.75f * scrW, 0.5f * scrH), "FullScreen"))
+            {
+                FullScreenToggle();
+            }
+            i++;
+            if (showRes)
+            {
+                GUI.Box(new Rect(0.75f * scrW, 3 * scrH + (i * (scrH * 0.5f)), 1.75f * scrW, 3.5f * scrH), "");
+
+                scrollPosRes = GUI.BeginScrollView(new Rect(0.75f * scrW, 3 * scrH + (i * (scrH * 0.5f)), 1.75f * scrW, 3.5f * scrH), scrollPosRes, new Rect(0, 0, 1.75f * scrW, 3.5f * scrH));
+
+                for (int resSize = 0; resSize < resX.Length; resSize++)
+                {
+                    if (GUI.Button(new Rect(0f * scrW, 0 * scrH + resSize * (scrH * 0.5f), 1.75f * scrW, 0.5f * scrH), resX[resSize].ToString() + "x" + resY[resSize].ToString()))
+                    {
+                        Screen.SetResolution(resX[resSize], resY[resSize], fullScreenToggle);
+                        showRes = false;
+                    }
+                }
+                GUI.EndScrollView();
+            }
+            #endregion
+
+        }
+    }
+
+    bool ToggleVolume()
+    {
+        if(muteToggle == true)
+        {
+            muteToggle = false;
+            volumeSlider = holdingVolume;
+            return false;
+        }
+        else
+        {
+            muteToggle = true;
+            holdingVolume = volumeSlider;
+            volumeSlider = 0;
+            mainMusic.volume = 0;
+            return true;
+        }
+    }
+
+    bool FullScreenToggle()
+    {
+        if(fullScreenToggle)
+        {
+            fullScreenToggle = false;
+            Screen.fullScreen = false;
+            return false;
+        }
+        else
+        {
+            fullScreenToggle = true;
+            Screen.fullScreen = true;
+            return true;
         }
     }
 
     // Use this for initialization
     void Start()
     {
-        scrW = Screen.width / 16;
+        scrW = Screen.width / 16;        
         scrH = Screen.height / 9;
+        fullScreenToggle = true;
+
+        brightness = GameObject.FindGameObjectWithTag("Sun").GetComponent<Light>();
+        mainMusic = GameObject.Find("MenuMusic").GetComponent<AudioSource>();
+        volumeSlider = mainMusic.volume;
+        brightnessSlider = brightness.intensity;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(mainMusic != null)
+        {
+            if(muteToggle == false)
+            {
+                if(mainMusic.volume != volumeSlider)
+                {
+                    holdingVolume = volumeSlider;
+                    mainMusic.volume = volumeSlider;
+                }
+            }
+            else
+            {
+                volumeSlider = 0;
+                mainMusic.volume = 0;
+            }
+        }
 
+        if (brightness != null)
+        {
+            if(brightnessSlider != brightness.intensity)
+            {
+                brightness.intensity = brightnessSlider;
+            }
+        }
     }
 }
